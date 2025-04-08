7ca70efff07f94b9bbb43f4830d90425e9992a90
Vsanotskyi <vsanotskyi@gmail.com>
2025-04-08 22:24:04 +0300

    fix hero image

--- src/components/Home/CompanyLogo.tsx
+++ src/components/Home/CompanyLogo.tsx
@@ -19,9 +19,9 @@ const CompanyLogo = () => {
 
     return (
         <div>
-            <div className="relative w-full h-screen">
+            <div className="relative w-full h-[490px] table:h-[700px] desktop:h-[810px]">
                 <div
-                    className="absolute inset-0 w-full h-[490px] table:h-[700px] desktop:h-[810px] bg-cover bg-center bg-no-repeat"
+                    className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
                     style={{backgroundImage: `url(${mainBg})`}}
                 />
                 <div className="relative z-10 flex flex-col items-center justify-center h-full">
