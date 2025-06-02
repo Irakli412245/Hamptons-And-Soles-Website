8f1c3c74db9c1ce79fb7c0306ed15d970dfe64f6
VSanotskyi <vsanotskyi@gmail.com>
2025-06-02 23:19:30 +0300

    add about fix hero bg

--- src/features/About/Hero.tsx
+++ src/features/About/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <div
       className={
         'bg-[url(/images/about/hero/about_hero_mob.png)] ' +
-        'table:bg-[url(public/images/about/hero/about_hero.png)] ' +
+        'table:bg-[url(/images/about/hero/about_hero.png)] ' +
         'w-full h-[700px] table:h-[810px] bg-cover bg-center bg-no-repeat ' +
         'flex flex-col justify-center items-center'
       }
